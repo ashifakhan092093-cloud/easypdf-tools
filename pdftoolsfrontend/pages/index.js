245b09e01c3ed02bbdb41da8bf68c3f698be92fc
ashifakhan092093-cloud <tumhara_github_email@example.com>
2025-12-11 00:42:06 +0530

    frontend build update

--- pdftoolsfrontend/pages/index.js
+++ pdftoolsfrontend/pages/index.js
@@ -1,10 +1,19 @@
-// pdftoolsfrontend/pages/index.js
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div style={{ padding: 40 }}>
+    <div>
       <h1>EasyPDF Tools</h1>
       <p>Select a tool from the menu.</p>
+
+      <ul>
+        <li><Link href="/merge-pdf/page">Merge PDF</Link></li>
+        <li><Link href="/compress-pdf/page">Compress PDF</Link></li>
+        <li><Link href="/pdf-split/page">Split PDF</Link></li>
+        <li><Link href="/pdf-to-word/page">PDF to Word</Link></li>
+        <li><Link href="/word-to-pdf/page">Word to PDF</Link></li>
+        {/* Baaki tools yaha */}
+      </ul>
     </div>
   );
 }
